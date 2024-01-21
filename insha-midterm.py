40ade1c6a687b7c2ab309d57e7dded911f6787a3
inshaasghar <156567165+inshaasghar@users.noreply.github.com>
2024-01-21 12:39:44 +0500

    Add files via upload

---
+++ insha-midterm.py
@@ -0,0 +1,37 @@
+#insha
+
+#Q1- Add two variable and print their addition and division,power,floordivision
+num1 = 5
+num2 = 4
+sum = num1+num2
+print ("the sum of",num1,"+",num2,"is", sum)
+
+num1 = 5
+num2 = 4
+division =num1/num2
+print ("the division of",num1,"/",num2,"is",division)
+
+num1 = 5
+num2 = 4
+power = num1^num2
+print ("the power of",num1,"^",num2,"is",power)
+
+num1 = 5
+num2 = 4
+floor = num1 // num2
+print ("the floor of",num1,"//",num2,"is",floor)
+
+#Q2 - find the area of a triangle
+l = 10
+h = 15
+area = 1/2 * l*h
+print ("the area of triangle",l,"*",h,"is",area)
+
+#Q4 - write a python program that returns all datatypes
+print(10)
+print("datatypes:",type(10))
+
+print(10.5)
+print("datatypes:",type(10.5))
+
+print("hello world")
